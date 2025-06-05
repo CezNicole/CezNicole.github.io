0f61675fd4f31ca945ae14908d89a8712f3f56c5
CezNicole <cezcomia@gmail.com>
2025-06-04 21:39:23 -0400

    Modify Travel Bloom images folder structure

--- scripts/travel-bloom-script.js
+++ scripts/travel-bloom-script.js
@@ -11,50 +11,55 @@ export async function loadData(){
     }
 }
 
+// WIP here
+const searchInput = document.getElementById('searchInput');
+
 export function search(){
-    const searchInput = document.getElementById('searchInput').value;
-    console.log(searchInput);
-    
-    if(searchInput){
-        console.log('Search Input is not empty');
+    /*
+    1. user enters destination / keyword from input field
+    2. search function functionality
+        a) on mobile: when magnifying icon is clicked, the form submits
+        b) on tablet/desktop: when Search button is clicked, the form submits
+    3. when user input matches the keyword on the JSON data, search results w/ the destination cards are displayed on the DOM
+        a) JSON data is filtered to display only the matched keyword / destination
+        b) dynamic DOM elements are created w/ styling
+    4. Timezone API functionality
+        a) if a country is searched, display the appropriate current local time from Timezone API
+        b) else, only show the aqua div container w/ no text
+
+    */
+
+    const searchText = searchInput.value.trim();
+    console.log(searchText);
+
+    if(!searchText){
         
-        loadData();
-        
-        destinations.forEach(destination => {
-            if(destination.name === searchInput){
-                console.log(destinations.filter(destination => destination.name === searchInput));
-                // const localTimezone = getLocalTimezone(destination.name);
-                // console.log(localTimezone);
-            }
-        })
-    
-
-        if(destinations.includes(searchInput)){
-            console.log(destinations.includes(searchInput));
-            
-            const searchResultsSection = document.createElement('section');
-            searchResultsSection.classList.add('search-results');
-
-            const localTimeDivContainer = document.createElement('div');
-            localTimeDivContainer.classList.add('search-results__local-time');
-            localTimeDivContainer.innerHTML = `Current Local Time ${getLocalTimezone(searchInput)}`;
-
-
-            searchResultsSection.appendChild(localTimeDivContainer);
-        }
+        searchInput.classList.add('error');
+        console.log('Please enter a valid destination / keyword.');
     } else{
-        searchResultsSection.innerHTML = 'No data found. Please modify your search.'
+        loadData();
     }
+    
+    // const form = document.querySelector('.search-bar');
+    // form.addEventListener('submit', (event) => {
+    //     event.preventDefault();
+
+        
+    // })
+
+
+
+
 }
 
-export async function getLocalTimezone(area){
-    try {
-        const response = await axios.get(`http://worldtimeapi.org/api/timezone/${area}`);
-        return response;
-    } catch (error) {
-        console.error('Error fetching timezone:', error.response?.status, error.message);
-    }
-}
+// export async function getLocalTimezone(area){
+//     try {
+//         const response = await axios.get(`http://worldtimeapi.org/api/timezone/${area}`);
+//         return response;
+//     } catch (error) {
+//         console.error('Error fetching timezone:', error.response?.status, error.message);
+//     }
+// }
 
 const btnSearchIcon = document.getElementById('btnMobileSearch');
 btnSearchIcon.addEventListener('click', (event) => {
