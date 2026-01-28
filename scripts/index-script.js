8a003a163fd9210bb98cafc1c162e66f06b6a142
CezNicole <cezcomia@gmail.com>
2026-01-28 17:54:12 -0500

    WIP - Refactor code to display Cybersecurity Projects as dynamic data on Portfolio website

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,4 +1,4 @@
-import {getProjectDetails, getProjects} from "./index-api.js";
+import {getProjects} from "./index-api.js";
 
 const btnContact = document.getElementById('btnContactMe');
 btnContact.addEventListener('click', (event) => {
@@ -46,7 +46,7 @@ const divSkillsContainer = document.querySelector('.section-cards__container--sk
 skills.forEach(skill => {
     const divSkills = document.createElement('div');
     divSkills.classList.add('section-cards__skills');
-    divSkills.innerHTML = skill;
+    divSkills.textContent = skill;
 
     divSkillsContainer.appendChild(divSkills);
 })
@@ -54,7 +54,6 @@ skills.forEach(skill => {
 
 // Refactor code to load and render Cybersecurity Projects
 let cybersecurityProjects = [];
-let cybersecurityProjectDetails = [];
 
 async function loadCybersecurityProjects(){
     try {
@@ -67,9 +66,9 @@ async function loadCybersecurityProjects(){
                 ...project
             }
         })
-        // console.log(projectWithIds);
+        console.log(projectWithIds);
         
-        renderAllProjects(projectWithIds);
+        // renderAllProjects(projectWithIds);
 
         return projectWithIds;
     } catch (error) {
@@ -81,239 +80,261 @@ async function loadCybersecurityProjects(){
 loadCybersecurityProjects();
 
 
-function renderAllProjects(projects){
+// WIP - Create DOM project cards
+function createProjectCard(project){
     const portfolioContainer = document.querySelector('.section-cards__container--portfolio');
 
-    projects.forEach(project => {
-        const divProjectDetails = document.createElement('div');
-        divProjectDetails.classList.add('project-details');
-        divProjectDetails.classList.add('project-details__cybersecurity');
+    const card = document.createElement('div');
+    card.classList.add('project-details', 'project-details__cybersecurity');
+    // divProjectDetails.classList.add('project-details__cybersecurity');
+
+    const projectName = document.createElement('h4');
+    projectName.classList.add('project-title');
+    projectName.textContent = project.projectName;
+
     
-        const projectTitle = document.createElement('h4');
-        projectTitle.classList.add('project-title');
-        projectTitle.innerHTML = project.title;
-    
-        const skillsList = document.createElement('ul');
-        skillsList.classList.add('highlighted-skills__list');
-    
-        project.skills.forEach(skill => {
-            const highlightedSkill = document.createElement('li');
-            highlightedSkill.classList.add('highlighted-skills');
-            highlightedSkill.innerHTML = skill;
+    const skillsList = createSkillsList(project.skills);
 
-            skillsList.appendChild(highlightedSkill);
-        })
 
-        const projectDesc = document.createElement('p');
-        projectDesc.classList.add('project-details__desc');
-        projectDesc.innerHTML = project.description;
+    const projectDesc = document.createElement('p');
+    projectDesc.classList.add('project-details__desc');
+    projectDesc.textContent = project.description;
 
-        const btnContainer = document.createElement('div');
-        btnContainer.classList.add('buttons__container');
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('buttons__container');
 
-        const btnViewDetails = document.createElement('button');
-        btnViewDetails.classList.add('buttons__project');
-        btnViewDetails.dataset.modal = project.id;
-        btnViewDetails.innerHTML = 'View Details';
+    const btnViewDetails = document.createElement('button');
+    btnViewDetails.classList.add('buttons__project');
+    btnViewDetails.dataset.modal = project.id;
+    btnViewDetails.textContent = 'View Details';
 
-        
-      
-        btnViewDetails.addEventListener('click', (event) => {
-            event.preventDefault();
-            openModal(project.id);
+    card.append(projectName, skillsList, projectDesc. btnContainer, btnViewDetails);
 
-            // hide <body> scroll functionality
-            document.body.classList.add('overflow-hidden');
-        })
+    return card;
+}
 
-        
-        // WIP - bug: clicking the button doesn't show project details
-        // const modal = document.getElementById(project.id);
-        // const btnClose = modal.querySelector('.close');
-        // modalFunctionality(btnViewDetails, btnClose, project.id);
-        
-        
-        divProjectDetails.appendChild(projectTitle);
-        divProjectDetails.appendChild(skillsList);
-        divProjectDetails.appendChild(projectDesc);
+function createSkillsList(skills){
+    const skillsList = document.createElement('ul');
+    skillsList.classList.add('highlighted-skills__list');
 
-        btnContainer.appendChild(btnViewDetails);
-        divProjectDetails.appendChild(btnContainer);
+    skills.forEach(skill => {
+        const highlightedSkill = document.createElement('li');
+        highlightedSkill.classList.add('highlighted-skills');
+        highlightedSkill.textContent = skill;
 
-        portfolioContainer.appendChild(divProjectDetails);
+        skillsList.appendChild(highlightedSkill);
+    })
+    return skillsList;
+}
+
+function createProjectModal(project){
+    const modal = document.createElement('div');
+    modal.id = project.id;
+    modal.classList.add('modal');
+
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal__content');
+
+    const btnClose = document.createElement('button');
+    btnClose.classList.add('close');
+    // btnClose.dataset.modal = project.id;
+    btnClose.textContent = 'X';
+
+    const title = document.createElement('h1');
+    title.classList.add('project-title', 'modal__titles');
+    // title.classList.add('modal__titles');
+    title.textContent = project.title;
+
+    const overviewDiv = document.createElement('div');
+    overviewDiv.classList.add('modal__header-content');
+
+    const overviewTitle = document.createElement('p');
+    overviewTitle.classList.add('project-details__desc--italic');
+    overviewTitle.textContent = 'Overview:';
+
+    const overviewContent = document.createElement('p');
+    overviewContent.classList.add('project-details__desc');
+    overviewContent.classList.add('project-details__desc--modal-padding');
+    overviewContent.textContent = project.overview;
+
+    overviewDiv.append(overviewTitle, overviewContent);
+    // overviewDiv.appendChild(overviewContent);
+
+
+    const roleDiv = document.createElement('div');
+    roleDiv.classList.add('modal__header-content');
+
+    const roleTitle = document.createElement('p');
+    roleTitle.classList.add('project-details__desc--italic');
+    roleTitle.textContent = 'Role:';
+
+    const roleContent = document.createElement('p');
+    roleContent.classList.add('project-details__desc', 'project-details__desc--modal-padding');
+    // roleContent.classList.add('project-details__desc--modal-padding');
+    roleContent.textContent = 'Cybersecurity Analyst';
+
+    roleDiv.append(roleTitle, roleContent);
+    // roleDiv.appendChild(roleContent);
+
+
+    const scenarioTitle = document.createElement('h2');
+    scenarioTitle.classList.add('modal__section-titles');
+    scenarioTitle.textContent = detail.sections[0].heading;
+
+    project.sections[0].content.forEach(item => {
+        const scenarioContent = document.createElement('p');
+        scenarioContent.classList.add('project-details__desc');
+        scenarioContent.textContent = item;
+        modalContent.appendChild(scenarioContent);
     })
 
-    return projects;
+    modalContent.append(btnClose, title, overviewDiv, roleDiv, scenarioTitle);
+    modal.appendChild(modalContent);
+
+    return modal;
 }
 
 
-// Cybersecurity Projects - Modal Functionality
-function openModal(id){
-    document.getElementById(id).classList.add('visible');
-}
+// function renderAllProjects(projects){
 
-function closeModal(id){
-    document.getElementById(id).classList.remove('visible');
-}
+       
+      
+//         btnViewDetails.addEventListener('click', (event) => {
+//             event.preventDefault();
+//             openModal(project.id);
 
-// document.querySelectorAll("[data-modal]").forEach(btnViewDetails => {
-//     btnViewDetails.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         const modalId = btnViewDetails.dataset.modal; //HTML5: Gets data-modal value
-//         openModal(modalId);
 
-//         // hide <body> scroll functionality
-//         document.body.classList.add('overflow-hidden');
-//     })    
-// });
+//             // hide <body> scroll functionality
+//             document.body.classList.add('overflow-hidden');
+//         })
 
-// document.querySelectorAll(".close[data-modal]").forEach(btnClose => {
-//     btnClose.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         const modalId = btnClose.dataset.modal; //HTML5: Gets data-modal value
-//         closeModal(modalId);
+        
 
-//         // make <body> scroll functionality visible
-//         document.body.classList.remove('overflow-hidden');
+//         btnContainer.appendChild(btnViewDetails);
+//         divProjectDetails.appendChild(btnContainer);
+
+//         portfolioContainer.appendChild(divProjectDetails);
 //     })
-// })
 
 
-// Refactor: btnViewDetails modal functionality
-// function modalFunctionality(openBtn, closeBtn, modalId){
-//     openBtn.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         openModal(modalId);
-
-//         // hide <body> scroll functionality
-//         document.body.classList.add('overflow-hidden');
-//     });
-
-//     closeBtn.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         closeModal(modalId);
-
-//         // make <body> scroll functionality visible
-//         document.body.classList.remove('overflow-hidden');
-//     })
 // }
 
 
-// Refactor: Load and Render Cybersecurity Project Details upon button click
-async function loadProjectDetails(){
-    try {
-        const response = await getProjectDetails();
-        cybersecurityProjectDetails = response;
+// // Cybersecurity Projects - Modal Functionality
+// function openModal(id){
+//     const modal = document.getElementById(id);
+//     if(!modal) return;
+//     modal.classList.add('visible');
+// }
+
+// function closeModal(id){
+//     const modal = document.getElementById(id);
+//     if(!modal) return;
+//     modal.classList.remove('visible');
+// }
+
+// // document.querySelectorAll("[data-modal]").forEach(btnViewDetails => {
+// //     btnViewDetails.addEventListener('click', (event) => {
+// //         event.preventDefault();
+// //         const modalId = btnViewDetails.dataset.modal; //HTML5: Gets data-modal value
+// //         openModal(modalId);
+
+// //         // hide <body> scroll functionality
+// //         document.body.classList.add('overflow-hidden');
+// //     })    
+// // });
+
+// // document.querySelectorAll(".close[data-modal]").forEach(btnClose => {
+// //     btnClose.addEventListener('click', (event) => {
+// //         event.preventDefault();
+// //         const modalId = btnClose.dataset.modal; //HTML5: Gets data-modal value
+// //         closeModal(modalId);
+
+// //         // make <body> scroll functionality visible
+// //         document.body.classList.remove('overflow-hidden');
+// //     })
+// // })
+
+
+// // Refactor: btnViewDetails modal functionality
+// // function modalFunctionality(openBtn, closeBtn, modalId){
+// //     openBtn.addEventListener('click', (event) => {
+// //         event.preventDefault();
+// //         openModal(modalId);
+
+// //         // hide <body> scroll functionality
+// //         document.body.classList.add('overflow-hidden');
+// //     });
+
+// //     closeBtn.addEventListener('click', (event) => {
+// //         event.preventDefault();
+// //         closeModal(modalId);
+
+// //         // make <body> scroll functionality visible
+// //         document.body.classList.remove('overflow-hidden');
+// //     })
+// // }
+
+
+// // Refactor: Load and Render Cybersecurity Project Details upon button click
+// async function loadProjectDetails(){
+//     try {
+//         const response = await getProjectDetails();
+//         cybersecurityProjectDetails = response;
         
-        const projectDetailsWithIds = cybersecurityProjectDetails.map((project, index) =>{
-            return{
-                id: `CSProject${index + 1}`,
-                ...project
-            }
-        })
-        // console.log(projectDetailsWithIds);
+//         const projectDetailsWithIds = cybersecurityProjectDetails.map((project, index) =>{
+//             return{
+//                 id: `CSProject${index + 1}`,
+//                 ...project
+//             }
+//         })
+//         // console.log(projectDetailsWithIds);
         
-        renderAllDetails(projectDetailsWithIds);
+//         renderAllDetails(projectDetailsWithIds);
 
-        return projectDetailsWithIds;
-    } catch (error) {
-        console.log('Error loading Cybersecurity Project Details:', error)        
-    }
-}
+//         return projectDetailsWithIds;
+//     } catch (error) {
+//         console.log('Error loading Cybersecurity Project Details:', error)        
+//     }
+// }
 
-loadProjectDetails();
-        
-
-// To-do: Display role": "Cybersecurity Analyst" for ALL PROJECTS (below Overview)
-
-
-
-
-function renderAllDetails(projectDetails){
-    projectDetails.forEach(detail => {
-        const parentContainer = document.createElement('div');
-        parentContainer.id = detail.id;
-        parentContainer.classList.add('modal');
-
-        const contentContainer = document.createElement('div');
-        contentContainer.classList.add('modal__content');
-
-        const btnClose = document.createElement('button');
-        btnClose.classList.add('close');
-        btnClose.dataset.modal = detail.id;
-        btnClose.textContent = 'X';
-
-
-        btnClose.addEventListener('click', (event) => {
-            event.preventDefault();
-            closeModal(detail.id);
-
-            // make <body> scroll functionality visible
-            document.body.classList.remove('overflow-hidden');
-        })
-
-
-        const title = document.createElement('h1');
-        title.classList.add('project-title');
-        title.classList.add('modal__titles');
-        title.innerHTML = detail.title;
-
-        const overviewDiv = document.createElement('div');
-        overviewDiv.classList.add('modal__header-content');
-
-        const overviewTitle = document.createElement('p');
-        overviewTitle.classList.add('project-details__desc--italic');
-        overviewTitle.innerHTML = 'Overview:';
-
-        const overviewContent = document.createElement('p');
-        overviewContent.classList.add('project-details__desc');
-        overviewContent.classList.add('project-details__desc--modal-padding');
-        overviewContent.innerHTML = detail.overview;
-
-        overviewDiv.appendChild(overviewTitle);
-        overviewDiv.appendChild(overviewContent);
-
-
-        const roleDiv = document.createElement('div');
-        roleDiv.classList.add('modal__header-content');
-
-        const roleTitle = document.createElement('p');
-        roleTitle.classList.add('project-details__desc--italic');
-        roleTitle.innerHTML = 'Role:';
-
-        const roleContent = document.createElement('p');
-        roleContent.classList.add('project-details__desc');
-        roleContent.classList.add('project-details__desc--modal-padding');
-        roleContent.innerHTML = 'Cybersecurity Analyst';
-
-        roleDiv.appendChild(roleTitle);
-        roleDiv.appendChild(roleContent);
-
-
-        const scenarioTitle = document.createElement('h2');
-        scenarioTitle.classList.add('modal__section-titles');
-        scenarioTitle.innerHTML = detail.sections[0].heading;
-
-        detail.sections[0].content.forEach((item) => {
-            const scenarioContent = document.createElement('p');
-            scenarioContent.classList.add('project-details__desc');
-            scenarioContent.innerHTML = item;
-        })
-
-
-
-        console.log(detail);
+// await loadProjectDetails();
         
 
-        contentContainer.appendChild(btnClose);
-        contentContainer.appendChild(title);
-        contentContainer.appendChild(overviewDiv);
-        contentContainer.appendChild(roleDiv);
-        contentContainer.appendChild(scenarioTitle);
-        contentContainer.appendChild(scenarioContent);
+// // To-do: Display role": "Cybersecurity Analyst" for ALL PROJECTS (below Overview)
 
 
-        parentContainer.appendChild(contentContainer);
-        document.body.appendChild(parentContainer);
-    })
-}
+
+
+// function renderAllDetails(projectDetails){
+//     projectDetails.forEach(detail => {
+//         
+
+
+//         btnClose.addEventListener('click', (event) => {
+//             event.preventDefault();
+//             closeModal(detail.id);
+
+//             // make <body> scroll functionality visible
+//             document.body.classList.remove('overflow-hidden');
+//         })
+
+
+//         
+
+
+
+//         console.log(detail);
+        
+
+//         contentContainer.appendChild(btnClose);
+//         contentContainer.appendChild(title);
+//         contentContainer.appendChild(overviewDiv);
+//         contentContainer.appendChild(roleDiv);
+//         contentContainer.appendChild(scenarioTitle);
+
+
+//         parentContainer.appendChild(contentContainer);
+//         document.body.appendChild(parentContainer);
+//     })
+// }
