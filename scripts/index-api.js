dfb1dba1351baa81328f114e013320dd919f250d
CezNicole <cezcomia@gmail.com>
2026-02-04 19:06:54 -0500

    Refactor code to display Cybersecurity Projects and highlighted skills as dynamic data on Portfolio page

--- scripts/index-api.js
+++ scripts/index-api.js
@@ -3,6 +3,15 @@ export async function getProjects(){
         const response = await axios.get('../scripts/data/cybersecurity-projects.json');
         return response.data.projects;
     } catch (error) {
-        console.log('Error fetching data', error);
+        console.log('Error fetching project data', error);
+    }
+}
+
+export async function getHighlightedSkills(){
+    try {
+        const response = await axios.get('../scripts/data/highlighted-skills.json');
+        return response.data.skills;
+    } catch (error) {
+        console.log('Error fetching skill data', error);
     }
 }
